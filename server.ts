c651f1fd290a645185f11f8fe83b9416641344d4
ahmed saad <mizoahmed017@gmail.com>
2025-04-07 17:42:05 +0200

    adjusted the server to use socket.io

--- server.ts
+++ server.ts
@@ -1,15 +1,52 @@
-import express, { Application } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./src/DB/dp";
 const port: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 import { app } from './src/index';
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
+
+const server = http.createServer(app);
+const io = new SocketIOServer(server, {
+  cors: {
+    origin: '*', 
+    methods: ['GET', 'POST'],
+  },
+});
+const connectedUsers = new Map<string, string>(); 
+
+io.on('connection', (socket) => {
+
+  socket.on('resgister', (userId) => {
+    connectedUsers.set(userId, socket.id);
+    console.log(`User ${userId} connected with socket ID: ${socket.id}`);
+    
+  });
+  socket.on('disconnect', () => {
+    connectedUsers.forEach((value, key) => {
+      if (value === socket.id) {
+        connectedUsers.delete(key);
+        console.log(`User ${key} disconnected`);
+      }
+    });
+  }
+  );
+
+    
+});
+
 connectDB()
   .then(() => {
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
   })
   .catch(() => {
-    "Error on connecting the server to the database";
+    console.log( "Error on connecting the server to the database");
   });
+
+
+
+
+
+export { io , connectedUsers }
