23991973a475abf25c2dd96e894d24e725cd2db6
ahmed saad <mizoahmed017@gmail.com>
2025-04-30 13:23:32 +0300

    added simple rate limiter to the app

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,17 @@ import path from "path";
 import cors from "cors";
 import helmet from "helmet";
 import { unCaughtErrorHandler } from "./util/errorHandling";
+import rateLimit from "express-rate-limit";
 const app: Application = express();
 
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per window
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
 
 const uploadDir = path.join(__dirname, 'public/images');
 if (!fs.existsSync(uploadDir)) {
@@ -20,6 +28,7 @@ if (!fs.existsSync(uploadDir)) {
       path.join("./logs/access.log"),
       { flags: "a" }
     );
+    app.use(limiter)
     // log all requests to access.log
     app.use(morgan("dev", { stream: accessLogStream }));
     // parse json to javascript object
